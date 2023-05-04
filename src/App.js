f64aab70f46d1eb0193ebb9b9dba69ea46f605b0
callmesveta <zhambalova_sveta@mail.ru>
2023-05-04 23:32:25 +0800

    added component Menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
+
+  let imgUrl = "https://static.tildacdn.com/tild3565-3139-4331-b366-666635393866/1645202220_33-kartin.png";
+
+  let menu = [
+    {
+        name: "Сырники",
+        ingredients: ["творог", "мука", "яйцо"],                           
+        price: 90
+    },
+
+    {
+        name: "Карбонара",
+        ingredients: ["паста", "бекон", "сливки", "сыр"],                         
+        price: 320
+    },
+
+    {
+        name: "Тосты с бананом",
+        ingredients: ["тост", "Нутелла", "банан"],                 
+        price: 55
+    }
+
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        {menu.map((item) => (
+          <Menu name={item.name} price={item.price}/>
+        ))}
+
+        <img src={imgUrl} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Menu/index.js
@@ -0,0 +1,9 @@
+const Menu = ({name, price}) => {
+    return (
+    <>
+        <p>Название: {name} | Цена: {price} руб</p>
+    </>
+    )
+};
+
+export default Menu;
